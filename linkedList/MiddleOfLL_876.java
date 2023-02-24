135fa2ac815aa4c5b706dd64a0a827376cd28e82
Archie Li <archieliyx@gmail.com>
2023-02-24 12:11:04 +1100

    add comments for ReverseLL_206 recur function

---
+++ linkedList/MiddleOfLL_876.java
@@ -0,0 +1,9 @@
+package linkedList;
+
+public class MiddleOfLL_876 {
+  public ListNode middleNode(ListNode head) {
+    ListNode newHead = null;
+    return newHead;    
+  }
+  
+}
--- linkedList/ReverseLL_206.java
+++ linkedList/ReverseLL_206.java
@@ -32,17 +32,16 @@ public class ReverseLL_206 {
   public static ListNode reverseLLRecur(ListNode head) {
     if (head == null || head.next == null) return head;
     ListNode curr = head.next;
+    // Reverse the original 1st node's next node to null
     head.next = null;
     ListNode newHead = reverseLLRecur(curr);
+    // After the original 1st node, every node's next node is reversed by its previous node after reverseLLRecur(previous node) returned
     curr.next = head;
     return newHead;    
   }
 
   public static void main(String[] args) {
     ListNode head = new ListNode(1, new ListNode(2, new ListNode(3)));
-    // head.printNode();
-    // head = reverseList(head);
-    // head.printNode();
     head = reverseLLRecur(head);
     head.printNode();
   }
