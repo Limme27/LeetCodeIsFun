13f66087b076b1750fe3d0c18ad33d24a65f2ba4
Archie Li <archieliyx@gmail.com>
2023-04-27 14:09:31 +1000

    Apr 4th

--- array/prefixSum/CountSubarraysWithKOdds_m1248.java
+++ array/prefixSum/CountSubarraysWithKOdds_m1248.java
@@ -1,9 +1,14 @@
 package array.prefixSum;
 
+import java.util.*;
+
 // 560 -> 1248
+// Not a Sliding window problem as fixing either left end or right end can not include all possible subarrays
+// For example, [0,0,0,1,0,0,1,0,0,0] where k = 2
 public class CountSubarraysWithKOdds_m1248 {
   public static int numberOfSubarrays(int[] nums, int k) {
     int count = 0;
+    /* 
     int oddCount = 0;
     int[] prefix = new int[nums.length + 1];
     prefix[0] = 1;
@@ -11,6 +16,22 @@ public class CountSubarraysWithKOdds_m1248 {
       oddCount += num % 2 == 1 ? 1 : 0;
       prefix[oddCount]++;
       if (oddCount >= k) count += prefix[oddCount - k];
+    }
+    */
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] % 2 == 0) nums[i] = 0;
+      else nums[i] = 1;
+    }
+    // (key: value) = (currSum: counter)
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    map.put(0, 1);
+    int sum = 0;
+    for (int num : nums) {
+      sum += num;
+      // If the target prefixSum (sum - k) exists in the map, just add up its count
+      if (map.containsKey(sum - k)) count += map.get(sum - k);
+      // Store the new sum in the map as the prefixSum for the remaining num
+      map.put(sum, map.getOrDefault(sum, 0) + 1);      
     }
     return count;
   }
--- array/slidingWindow/LgstNiceSubarray_2401.java
+++ array/slidingWindow/LgstNiceSubarray_2401.java
@@ -15,5 +15,4 @@ public class LgstNiceSubarray_2401 {
     }
     return maxLen;
   }
-  
 }
